b6147d22e2eeb15f70d5ea6c24395f0bde324b37
qijianbei <17600045817@163.com>
2018-04-10 15:24:13 +0800

    收货地址本地渲染+删除事件

--- src/components/MineList/MyAddress.vue
+++ src/components/MineList/MyAddress.vue
@@ -9,52 +9,10 @@
       </div>
 
       <div class="main">
-          <ul class="list">
-              <li>
-                  <span class="name">皮皮虾</span>
-                  <span class="tel">12312321312</span>
-              </li>
-              <li class="address">
-                  武汉市武汉市未来大道
-              </li>
-              <li>
-                  <span>       
-                       <input type="radio" id="adress-01" name="sex" checked/>
-                       <label for="adress-01"></label>
-                        默认地址
-                        <p>
-                            <img src="../../assets/manage/编辑.png" alt="">编辑
-                           <b><img src="../../assets/manage/删除.png" alt="">删除</b> 
-                        </p>
-                  </span>   
-              </li>
-          </ul>
-
-          <ul class="list">
-              <li>
-                  <span class="name">大闸蟹</span>
-                  <span class="tel">23423423423</span>
-              </li>
-              <li class="address">
-                  河北省河北省河北省
-              </li>
-              <li>
-                  <span>       
-                       <input type="radio" id="adress-02" name="sex"/>
-                       <label for="adress-02"></label>
-                        默认地址
-                        <p>
-                            <img src="../../assets/manage/编辑.png" alt="">编辑
-                            <b><img src="../../assets/manage/删除.png" alt="">删除</b> 
-                        </p>
-                  </span>   
-              </li>
-          </ul>
-      
-         <ul>
-              <li>
-                  <span class="name">大闸蟹</span>
-                  <span class="tel">23423423423</span>
+         <ul v-for="item in items" class="list">
+             <li>
+                  <span class="name">{{item.name}}</span>
+                  <span class="tel">{{item.tel}}</span>
               </li>
               <li class="address">
                   河北省河北省河北省
@@ -66,33 +24,12 @@
                         默认地址
                         <p>
                             <img src="../../assets/manage/编辑.png" alt="">编辑
-                            <b><img src="../../assets/manage/删除.png" alt="">删除</b>
+                            <b @click="display(item)"><img src="../../assets/manage/删除.png" alt="">删除</b>
                         </p>
                   </span>   
               </li>
          </ul>
       
-        <ul>
-              <li>
-                  <span class="name">大闸蟹</span>
-                  <span class="tel">23423423423</span>
-              </li>
-              <li class="address">
-                  河北省河北省河北省
-              </li>
-              <li>
-                  <span>       
-                       <input type="radio" id="adress-04" name="sex"/>
-                       <label for="adress-04"></label>
-                        默认地址
-                        <p>
-                            <img src="../../assets/manage/编辑.png" alt="">编辑
-                            <b><img src="../../assets/manage/删除.png" alt="">删除</b>
-                        </p>
-                  </span>   
-              </li>
-        </ul>
-            
       </div>
       <div class="bottom">
            <router-link :to="{ path: '/AddAddress' }" tag='button'>＋新建地址</router-link>
@@ -112,246 +49,241 @@
                确定删除地址吗？
            </div>
            <p>
-               <span class="no">取消</span>
-               <span class="yes">确定</span>
+               <span class="no" @click="cancel">取消</span>
+               <span class="yes" @click="q">确定</span>
            </p>
        </div>     
   </div>
 </template>
 <script>
-import AddAddress from '../MineList/AddAddress'
-import { Header,Cell,Actionsheet,Popup } from 'mint-ui';
+import AddAddress from "../MineList/AddAddress";
+import { Header, Cell, Actionsheet, Popup } from "mint-ui";
 export default {
-   data(){
-       return{
-          EditAddress:false,
-       }  
-   },
-   methods:{
-      closeAddress:function(){
-          this.$parent.$parent.address = false
-      },
-      EditAddressOpen:function(){
-          this.EditAddress = true;
-      }
-   },
-   components:{
-       AddAddress
-   },
-   mounted:function(){
-       $(".no").click(function(){
-            $('.del').hide()
-            var a = $(".main b").text()
-            console.log(a)
-        //     var res = a.forEach(function(item, index, input) {
-        //     console.log(item);
-        //     console.log(index);
-        //     console.log(input);
-        // })
-        //   function a(item, index) {
-        //         // demoP.innerHTML = demoP.innerHTML + "index[" + index + "]: " + item + "<br>"; 
-        //         console.log(item)
-        //         console.log(index)
-        //     }
-           
-        });
-       $(".list b").click(function(){
-            $('.del').show()
-            var b = $(this).text()
-            $(".yes").click(function(){
-                console.log(b)
-            //    b.parent().remove();
-            })
-        });
-   }
-} 
+  data() {
+    return {
+      EditAddress: false,
+      items: [
+        { name: "刘一", tel: "1234566789" },
+        { name: "陈二", tel: "1234566789" },
+        { name: "张三", tel: "1234566789" },
+        { name: "李四", tel: "1234566789" },
+        { name: "王五", tel: "1234566789" }
+      ],
+      init: null
+    };
+  },
+  methods: {
+    closeAddress: function() {
+      this.$parent.$parent.address = false;
+    },
+    EditAddressOpen: function() {
+      this.EditAddress = true;
+    },
+    //删除地址事件
+    display(e) {
+      this.init = e;
+      $(".del").show();
+    },
+    //取消事件
+    cancel() {
+      $(".del").hide();
+    },
+    //确定事件
+    q() {
+      let index = this.items.indexOf(this.init);
+      this.items.splice(index, 1);
+      $(".del").hide();
+    }
+  },
+  components: {
+    AddAddress
+  },
+  mounted: function() {
+  }
+};
 </script>
 
 
 <style scoped>
-html,body{
-    width: 100%;
-    height: 100%;
-    display: flex;
+html,
+body {
+  width: 100%;
+  height: 100%;
+  display: flex;
 }
-    .manage{
-       width: 100%;
-       height: 100%;  
-    }
-    .header{
-        width: 100%;
-        height: .96rem;
-        background: #fff;
-        position: absolute;
-        top: 0;
-        border-bottom: 1px solid #e7e7e7;
-    
-    }
-    .header ul{
-        display: flex;
-    }
-    .header li{
-        flex: 1;
-        line-height: .96rem;
-        text-align: center
-    }
-    .header li:nth-child(1){
-        text-align: left;
-        padding-left: .3rem;
-         font-size: .32rem;
-    }
-    .header li:nth-child(2){
-        font-size: .32rem;
-    }
-    .main{
-        flex: 1;
-        width: 100%;
-        height: 100%;
+.manage {
+  width: 100%;
+  height: 100%;
+}
+.header {
+  width: 100%;
+  height: 0.96rem;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  border-bottom: 1px solid #e7e7e7;
+}
+.header ul {
+  display: flex;
+}
+.header li {
+  flex: 1;
+  line-height: 0.96rem;
+  text-align: center;
+}
+.header li:nth-child(1) {
+  text-align: left;
+  padding-left: 0.3rem;
+  font-size: 0.32rem;
+}
+.header li:nth-child(2) {
+  font-size: 0.32rem;
+}
+.main {
+  flex: 1;
+  width: 100%;
+  height: 100%;
 
-        overflow: auto;
-    }
-    .main ul{
-     
-        margin-top: .2rem;
-    }
-    .main ul:nth-child(1){
-         margin-top: .96rem;
-    }
-    .main ul:last-child{
-        margin-bottom: .96rem
-    }
-    .main li{
-        background: #fff;
-    }
-    .main li:nth-child(1){
-        padding-top: .35rem;
-       
-    }
-    .main li:nth-child(1) span{
-        font-size: .28rem;
-    }
-    .name{
-        padding-left: .3rem;  
-    }
-    .tel{
-        padding-left: .5rem;
-
-    }
-    .main li:nth-child(2){
-        font-size: .24rem;
-        color: #667766;
-        padding-left: .3rem;
-        padding-top: .25rem;
-        padding-bottom: .35rem;
-        border-bottom: 1px solid #e7e7e7
-    }
-    .main li:nth-child(3){
-        height: .8rem;
-        line-height: .8rem;
-        padding-left: .3rem;
-        color: #676767;
-        font-size: .24rem;
-        position: relative;
-    }
-    .main li:nth-child(3) input{
-        display: none;
-        padding-left: .3rem;
-    }
-    .main li:nth-child(3) p{
-        display: inline-block;
-        width: 80%;
-        position: absolute;
-        right: 0;
-        text-align: right ;
-        padding-right: .3rem;
-    }
-    .main li:nth-child(3) p img{
-        vertical-align: middle;
-        width: .5rem;
-        height: .5rem;
-        margin-top: -.1rem;
-        margin-right: .2rem;
-    }
-    .main li:nth-child(3) p img:nth-child(2){
-        margin-left: .5rem;
-    }
-    input[type="radio"] + label::before {
-        box-sizing: border-box;
-        content: " "; /*不换行空格*/
-        display: inline-block;
-        vertical-align: middle;
-        width: 2em;
-        height: 2em;
-        background: url("../../assets/manage/change_no.png");
-        background-size: 100% 100%;
-        /* margin-right: .4em; */
-        border-radius: 50%;
-        margin-top: -.1rem
+  overflow: auto;
+}
+.main ul {
+  margin-top: 0.2rem;
+}
+.main ul:nth-child(1) {
+  margin-top: 0.96rem;
+}
+.main ul:last-child {
+  margin-bottom: 0.96rem;
+}
+.main li {
+  background: #fff;
+}
+.main li:nth-child(1) {
+  padding-top: 0.35rem;
+}
+.main li:nth-child(1) span {
+  font-size: 0.28rem;
+}
+.name {
+  padding-left: 0.3rem;
+}
+.tel {
+  padding-left: 0.5rem;
+}
+.main li:nth-child(2) {
+  font-size: 0.24rem;
+  color: #667766;
+  padding-left: 0.3rem;
+  padding-top: 0.25rem;
+  padding-bottom: 0.35rem;
+  border-bottom: 1px solid #e7e7e7;
+}
+.main li:nth-child(3) {
+  height: 0.8rem;
+  line-height: 0.8rem;
+  padding-left: 0.3rem;
+  color: #676767;
+  font-size: 0.24rem;
+  position: relative;
+}
+.main li:nth-child(3) input {
+  display: none;
+  padding-left: 0.3rem;
+}
+.main li:nth-child(3) p {
+  display: inline-block;
+  width: 80%;
+  position: absolute;
+  right: 0;
+  text-align: right;
+  padding-right: 0.3rem;
+}
+.main li:nth-child(3) p img {
+  vertical-align: middle;
+  width: 0.5rem;
+  height: 0.5rem;
+  margin-top: -0.1rem;
+  margin-right: 0.2rem;
+}
+.main li:nth-child(3) p img:nth-child(2) {
+  margin-left: 0.5rem;
+}
+input[type="radio"] + label::before {
+  box-sizing: border-box;
+  content: " "; /*不换行空格*/
+  display: inline-block;
+  vertical-align: middle;
+  width: 2em;
+  height: 2em;
+  background: url("../../assets/manage/change_no.png");
+  background-size: 100% 100%;
+  /* margin-right: .4em; */
+  border-radius: 50%;
+  margin-top: -0.1rem;
 }
 input[type="radio"]:checked + label::before {
-    /* background-color: #909194;
+  /* background-color: #909194;
     background-clip: content-box; */
-    background: red;
-    background: url("../../assets/manage/change.png");
-    
-    background-size: 100% 100%;
+  background: red;
+  background: url("../../assets/manage/change.png");
+
+  background-size: 100% 100%;
 }
-button{
-    display: inline-block;
-    width: 90%;
-    height: .8rem;
-    border: none;
-    border-radius: .2rem;
-    color: #fff;
-    margin-left: 5%;
-    background: #ff0103;
+button {
+  display: inline-block;
+  width: 90%;
+  height: 0.8rem;
+  border: none;
+  border-radius: 0.2rem;
+  color: #fff;
+  margin-left: 5%;
+  background: #ff0103;
 }
-.bottom{
-    width: 100%;
-    height: .96rem;
-    background: #fff;
-    position: absolute;
-    bottom: 0;
+.bottom {
+  width: 100%;
+  height: 0.96rem;
+  background: #fff;
+  position: absolute;
+  bottom: 0;
 }
-.del{
-    display: none;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,.5);
-    z-index: 999;
-    text-align: center;
+.del {
+  display: none;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  text-align: center;
 }
-.del div{
-    width: 70%;
-    height: 2rem;
-    line-height: 2rem;
-    background: #fff;
-    text-align: center;
-    margin: auto;
-    margin-top:60%;
-    border-top-left-radius: .2rem;
-    border-top-right-radius: .2rem;
+.del div {
+  width: 70%;
+  height: 2rem;
+  line-height: 2rem;
+  background: #fff;
+  text-align: center;
+  margin: auto;
+  margin-top: 60%;
+  border-top-left-radius: 0.2rem;
+  border-top-right-radius: 0.2rem;
 }
-.del p{
-    width: 70%;
-    background: #fff;
-    height: .7rem;
-    line-height: .7rem;
-    display: flex;
-    margin: auto;
-    border-top: 1px solid #e7e7e7;
-    border-bottom-left-radius: .2rem;
-    border-bottom-right-radius: .2rem;
-    overflow: hidden;
+.del p {
+  width: 70%;
+  background: #fff;
+  height: 0.7rem;
+  line-height: 0.7rem;
+  display: flex;
+  margin: auto;
+  border-top: 1px solid #e7e7e7;
+  border-bottom-left-radius: 0.2rem;
+  border-bottom-right-radius: 0.2rem;
+  overflow: hidden;
 }
-.del span{
-    flex: 1;
+.del span {
+  flex: 1;
 }
-.del span:last-child{
-    background: #ff0103;
-    color: #fff;
-    
+.del span:last-child {
+  background: #ff0103;
+  color: #fff;
 }
 </style>
