afbac5959a65f26935888b5f66a194481df018cf
qi jianbei <17600045817@163.com>
2018-04-17 03:01:23 +0800

    余额充值中心

--- src/components/Sher/Topup.vue
+++ src/components/Sher/Topup.vue
@@ -26,7 +26,7 @@
                 <p>支付宝</p>
                 <p>因为信任,所以简单</p>
                 <span>
-                    <input type="radio" id="zhifubao" name="sex" value="支付宝" checked/>
+                    <input type="radio" class="1" id="zhifubao" name="sex" value="1" checked/>
                     <label for="zhifubao"></label>
                 </span>
             </li>
@@ -67,229 +67,343 @@
     
 </template>
 <script>
-import $ from 'jquery';
+import $ from "jquery";
+import { setCookie, getCookie } from "../../assets/js/cookie.js";
 
- 
 export default {
-   mounted:function(){
-    $('.main_top li').on("click",function(){
-        $(this).addClass('col').siblings().removeClass('col')     
+  mounted: function() {
+    $(".main_top li").on("click", function() {
+      $(this)
+        .addClass("col")
+        .siblings()
+        .removeClass("col");
     });
     $("#money").focus(function() {
-        $(".main_top li").removeClass('col')       
+      $(".main_top li").removeClass("col");
     });
 
+    //     $(".main_top").find("li").each(function () {
+    //         $(this).click(function () {
+    //        var monval = $(this).val();
 
-//     $(".main_top").find("li").each(function () {
-//         $(this).click(function () {
-//        var monval = $(this).val();
-       
+    //         })
+    //        console.log(monval)
+    // })
 
+    //     $(".main_top").find("li").each(function () {
 
-       
-//         })
-//        console.log(monval)
-// })
+    // })
+    var accessToken = getCookie("access_token");
 
-//     $(".main_top").find("li").each(function () {
-       
-       
-// })
-      $("#btn").on('click',function(){ 
-         if($('#money').val()== ''){
-                $(".main_top").find("li").each(function () {
+    $("#btn").on("click", function() {
+      if ($("#money").val() == "") {
+        $(".main_top")
+          .find("li")
+          .each(function() {
             //  console.log($(this).val())
-                if($(this).is('.col')){
-                  var re=$('input:radio[name="sex"]:checked').val();
-                  console.log('即将使用'+re+'为您充值'+$(this).val()+'元')                 
-               };
-              })
-           }else{
-              var re=$('input:radio[name="sex"]:checked').val();
-              console.log('即将使用'+re+'为您充值'+$('#money').val()+'元');
-              $("#money").val('') 
-          }
+            if ($(this).is(".col")) {
+              var re = $('input:radio[name="sex"]:checked').val();
+              var mo = $(this).val();
+            //   console.log(mo);
+            //   console.log("即将使用" + re + "为您充值" + $(this).val() + "元");
+              //var data = {"aliases":this.name,"phone":this.phone,"contact":this.contact}
+              var data = { payType: re, payment: mo };
           
-      });  
-    // var re=$('input:radio[name="sex"]:checked').val();
-    // console.log(re)  
-  },
+              // this.$axios.post('http://cloud.eyun.online:9080/order/api/dep-orders/deposit',data,{
+              //     headers:{
+              //          'Authorization': 'Bearer ' + accessToken,
+              //     }
+              //     })
+              // .then(function(res){
+              //     console.log(res)
+              // })
+              // .catch(function(error){
+              //     console.log(error)
+              // })
+              $.ajax({
+                url:
+                  "http://cloud.eyun.online:9080/order/api/dep-orders/deposit",
+                // method:'post',
+                type: "POST",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify(data),
+                // data : datas,
+                headers: {
+                  Authorization: "Bearer " + accessToken
+                  //   'Content-Type':"application/json"
+                },
+                success: function(res) {
+                  console.log(res);
+                  var param1 = 1;
+                  var param2 = "orderStr" + ":" + res.orderString;
+                  var param3 = res.orderString;
+                 // var paytype = Ali;
+                  //top
 
-}
+                  var val = {
+                    func: "pay",
+                    param: {
+                        payType:"Ali",
+                        orderStr: param3,
+
+                    }
+                  };
+                    console.log(val)
+                     console.log(JSON.stringify(val))
+                  var u = navigator.userAgent;
+                  var isAndroid =
+                    u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; // android终端
+                  var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+                  if (isiOS) {
+                    window.webkit.messageHandlers.GongrongAppModel.postMessage(
+                      val
+                    );
+                  } else if (isAndroid) {
+                    window.androidObject.JSCallAndroid(JSON.stringify(val));
+                  }
+               
+                  //bottom
+                },
+                error(res) {
+                  console.log(res);
+                }
+              });
+            }
+          });
+      } else {
+        var re = $('input:radio[name="sex"]:checked').val();
+        console.log("即将使用" + re + "为您充值" + $("#money").val() + "元");
+        var mm = $("#money").val();
+        var data = { payType: re, payment: mm };
+        $.ajax({
+          url: "http://cloud.eyun.online:9080/order/api/dep-orders/deposit",
+          // method:'post',
+          type: "POST",
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify(data),
+          // data : datas,
+          headers: {
+            Authorization: "Bearer " + accessToken
+            //   'Content-Type':"application/json"
+          },
+          success: function(res) {
+               console.log(res);
+                  var param1 = 1;
+                  var param2 = "orderStr" + ":" + res.orderString;
+                  var param3 = res.orderString;
+                 // var paytype = Ali;
+                  //top
+
+                  var val = {
+                    func: "pay",
+                    param: {
+                        payType:"Ali",
+                        orderStr: param3,
+
+                    }
+                  };
+                    console.log(val)
+                     console.log(JSON.stringify(val))
+                  var u = navigator.userAgent;
+                  var isAndroid =
+                    u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; // android终端
+                  var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+                  if (isiOS) {
+                    window.webkit.messageHandlers.GongrongAppModel.postMessage(
+                      val
+                    );
+                  } else if (isAndroid) {
+                    window.androidObject.JSCallAndroid(JSON.stringify(val));
+                  }
+               
+                  //bottom
+          },
+          error(res) {
+            console.log(res);
+          }
+        });
+        $("#money").val("");
+      }
+    });
+    // var re=$('input:radio[name="sex"]:checked').val();
+    // console.log(re)
+  }
+};
 </script>
 
 <style scoped>
-  
-    .header{
-        width: 100%;
-        height: .96rem;
-        border-bottom: 1px solid #e7e7e7;   
-    }
-    .header ul{
-        display: flex;
-    }
-    .header li{
-        flex: 1;
-        height: .96rem;
-        line-height: .96rem;
-        background: #fff;
-        text-align: center;
-        font-size:.32rem;
-    }
-    .header li:nth-child(1){
-        text-align: left;      
-    }
-    .header span{
-        padding-left: .3rem;
-    }
-    .main{
-        text-align: center
-    }
-    .main_top{
-        width: 100%;
-        padding-bottom: .4rem;
-        background: #fff;  
-    }
-    .main_top input{
-        width: 6.1rem;
-        margin-left: .3rem;
-        height: .66rem;
-        /* margin-left: .4rem; */
-        border-radius: .1rem;
-        margin-right: .4rem;
-        margin-top: .4rem;
-        padding-left: .3rem;
-        border: 2px solid #e7e7e7;
-    }
-    input::-webkit-input-placeholder{ /*WebKit browsers*/
-        color: #c4c4c4;
-    }
-    .main_top li{
-        width: 1.95rem;
-        background: #f2f2f2;
-        text-align: center;
-        height: .88rem;
-        line-height: .88rem;
-        box-sizing: border-box;
-        float: left;
-        margin-left: 6%;
-        text-align: center;
-        margin-top: .4rem;
-        border-radius: .2rem;
-    }
-    .col{
-       background: #fff;
-       color: #ff5064;
-       border:1px solid #ff5064;
-    
-   }
-    .main p{
-        margin-top: .2rem;
-        margin-bottom: .2rem;
-        text-align: left;
-        padding-left: .4rem;
-        font-size: .28rem;
-        color: #c4c4c4
-    }
-    .pay{
-        width: 100%;
-        height:4rem;
-        overflow: auto;
-    }
-    .pay ul:last-child{
-        margin-bottom:3rem;
-    }
-    .top_bottom{
-        background: #fff;
-    }
-    .top_bottom1{
-        width: 100%;
-        height: .78rem;
-        background: #fff;
-        position: relative;
-        margin-top: .02rem;
-        padding-top: .1rem;
-        padding-bottom: .3rem;
-    }
-    .top_bottom1 li{
-        width: 50%;
-        text-align: left;
-        float: left;
-        line-height: .96rem;
-        height: .96rem;
-        background: #fff;
-    }
+.header {
+  width: 100%;
+  height: 0.96rem;
+  border-bottom: 1px solid #e7e7e7;
+}
+.header ul {
+  display: flex;
+}
+.header li {
+  flex: 1;
+  height: 0.96rem;
+  line-height: 0.96rem;
+  background: #fff;
+  text-align: center;
+  font-size: 0.32rem;
+}
+.header li:nth-child(1) {
+  text-align: left;
+}
+.header span {
+  padding-left: 0.3rem;
+}
+.main {
+  text-align: center;
+}
+.main_top {
+  width: 100%;
+  padding-bottom: 0.4rem;
+  background: #fff;
+}
+.main_top input {
+  width: 6.1rem;
+  margin-left: 0.3rem;
+  height: 0.66rem;
+  /* margin-left: .4rem; */
+  border-radius: 0.1rem;
+  margin-right: 0.4rem;
+  margin-top: 0.4rem;
+  padding-left: 0.3rem;
+  border: 2px solid #e7e7e7;
+}
+input::-webkit-input-placeholder {
+  /*WebKit browsers*/
+  color: #c4c4c4;
+}
+.main_top li {
+  width: 1.95rem;
+  background: #f2f2f2;
+  text-align: center;
+  height: 0.88rem;
+  line-height: 0.88rem;
+  box-sizing: border-box;
+  float: left;
+  margin-left: 6%;
+  text-align: center;
+  margin-top: 0.4rem;
+  border-radius: 0.2rem;
+}
+.col {
+  background: #fff;
+  color: #ff5064;
+  border: 1px solid #ff5064;
+}
+.main p {
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
+  text-align: left;
+  padding-left: 0.4rem;
+  font-size: 0.28rem;
+  color: #c4c4c4;
+}
+.pay {
+  width: 100%;
+  height: 4rem;
+  overflow: auto;
+}
+.pay ul:last-child {
+  margin-bottom: 3rem;
+}
+.top_bottom {
+  background: #fff;
+}
+.top_bottom1 {
+  width: 100%;
+  height: 0.78rem;
+  background: #fff;
+  position: relative;
+  margin-top: 0.02rem;
+  padding-top: 0.1rem;
+  padding-bottom: 0.3rem;
+}
+.top_bottom1 li {
+  width: 50%;
+  text-align: left;
+  float: left;
+  line-height: 0.96rem;
+  height: 0.96rem;
+  background: #fff;
+}
 
-    .top_bottom1 li:nth-child(2){
-        line-height: .45rem;
-        position: relative;
-    }
-    .top_bottom1 li:nth-child(2) p:nth-child(2){
-        margin-top: .3rem;
-    }
-    .top_bottom1 img{
-        width: 10%;
-        left: .4rem;
-        margin-top: .15rem;
-        position: absolute;
-    }
-    .top_bottom1 p{
-        line-height: .2rem;
-        font-size: .32rem;
-        margin-left: -2.5rem;
-    }
-    .top_bottom1 p:nth-child(1){
-        color: #2f2f2f;
-    }
-        input[type="radio"] + label::before {
-        box-sizing: border-box;
-        content: " "; /*不换行空格*/
-        display: inline-block;
-        vertical-align: middle;
-        width: 2em;
-        height: 2em;
-        background: url("../../assets/manage/change_no.png");
-        background-size: 100% 100%;
-        /* margin-right: .4em; */
-        border-radius: 50%;
-        margin-top: -.1rem
-    }
-    input[type="radio"]:checked + label::before {
-        background: red;
-        background: url("../../assets/manage/change.png");
-        
-        background-size: 100% 100%;
-    }
-    .top_bottom1 input{
-        display: none
-    }
-    .top_bottom1 span{
-        display: inline-block;
-        position: absolute;
-        line-height: .45rem;    
-        right: .5rem;
-        top: .25rem;
-    }
-    .jump{
-        width: 100%;
-        height: 1rem;
-        background: #ccc;
-        opacity: 0;
-    }
-    .buttom{
-       width: 100%;
-        height: 1rem;
-       background: #fff;
-       position: absolute;
-      bottom: 0;   
-     }
-     button{
-       width: 100%;
-       height: 1rem;
-       border:none;
-       color: #fff;
-       font-size: .32rem;
-       background: #ff5064;
-    }
+.top_bottom1 li:nth-child(2) {
+  line-height: 0.45rem;
+  position: relative;
+}
+.top_bottom1 li:nth-child(2) p:nth-child(2) {
+  margin-top: 0.3rem;
+}
+.top_bottom1 img {
+  width: 10%;
+  left: 0.4rem;
+  margin-top: 0.15rem;
+  position: absolute;
+}
+.top_bottom1 p {
+  line-height: 0.2rem;
+  font-size: 0.32rem;
+  margin-left: -2.5rem;
+}
+.top_bottom1 p:nth-child(1) {
+  color: #2f2f2f;
+}
+input[type="radio"] + label::before {
+  box-sizing: border-box;
+  content: " "; /*不换行空格*/
+  display: inline-block;
+  vertical-align: middle;
+  width: 2em;
+  height: 2em;
+  background: url("../../assets/manage/change_no.png");
+  background-size: 100% 100%;
+  /* margin-right: .4em; */
+  border-radius: 50%;
+  margin-top: -0.1rem;
+}
+input[type="radio"]:checked + label::before {
+  background: red;
+  background: url("../../assets/manage/change.png");
+
+  background-size: 100% 100%;
+}
+.top_bottom1 input {
+  display: none;
+}
+.top_bottom1 span {
+  display: inline-block;
+  position: absolute;
+  line-height: 0.45rem;
+  right: 0.5rem;
+  top: 0.25rem;
+}
+.jump {
+  width: 100%;
+  height: 1rem;
+  background: #ccc;
+  opacity: 0;
+}
+.buttom {
+  width: 100%;
+  height: 1rem;
+  background: #fff;
+  position: absolute;
+  bottom: 0;
+}
+button {
+  width: 100%;
+  height: 1rem;
+  border: none;
+  color: #fff;
+  font-size: 0.32rem;
+  background: #ff5064;
+}
 </style>
 
