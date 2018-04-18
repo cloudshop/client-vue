1a5a8c9d805f869a5bdfc763170ad35b98458c3f
qi jianbei <17600045817@163.com>
2018-04-18 21:10:07 +0800

    退出登录

--- src/components/MineList/Information.vue
+++ src/components/MineList/Information.vue
@@ -116,7 +116,16 @@ export default {
               refreshAll: true
             }
           };
-          window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+          var u = navigator.userAgent;
+          var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; // android终端
+          var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+          if (isiOS) {
+            window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+          } else if (isAndroid) {
+            window.androidObject.JSCallAndroid(JSON.stringify(val));
+          }
+        
+          console.log(val);
         },
         error(res) {
           console.log(res);
