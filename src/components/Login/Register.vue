74e55f885a4bbf58da74648b307037c88162c30e
zhang jingjing <13691226692@163.com>
2018-04-24 07:52:39 +0000

    Update Register.vue

--- src/components/Login/Register.vue
+++ src/components/Login/Register.vue
@@ -51,7 +51,8 @@ export default {
           var recommends=document.getElementById("recommends").value; 
           var authCode=document.getElementById("authCode").value; 
           var p1=/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
-          setCookie('authCode',authCode,1000*60)
+          setCookie('authCode',authCode)
+          setCookie('recommends',recommends)
           //(p1.test(theinput)); 
           var that = this;
           if(p1.test(recommends) != "") { 
