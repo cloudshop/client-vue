10bc80b0a2b344cf2c3b404ed3aa7cc9fdbeef62
zhang jingjing <13691226692@163.com>
2018-04-25 09:26:32 +0000

    login 路径

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -85,7 +85,7 @@ export default {
     },
     btn() {
       var data = {'username':this.PassName,'password':this.PassWord}
-      this.$axios.post('http://cloud.eyun.online:9080/auth/login',data)
+      this.$axios.post('http://cloud.eyun.online:9080/auth/login/app',data)
       .then(function(response) {
           var  val={
               "func":"closeCurrent",
