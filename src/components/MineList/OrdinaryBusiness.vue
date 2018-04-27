dc7ac926222ea1429fdf53a1762937beb017644b
qi jianbei <17600045817@163.com>
2018-04-27 22:12:27 +0800

    商户照片上传

--- src/components/MineList/OrdinaryBusiness.vue
+++ src/components/MineList/OrdinaryBusiness.vue
@@ -16,172 +16,344 @@
 
             <div class='main'>
                <h2>本人身份证照片</h2>
-               <div class='self'>    
-                     <div class='list'>
-                        <input type="file" class='file'>
-                        <div class='relative'>
-                            <img src="../../assets/Mine/相机.png" alt="">
-                            <span>身份证正面</span>
-                        </div>                       
-                     </div>
-                    <div class='list'>
-                        <input type="file"  class='file'>
-                         <div class='relative'>
-                            <img src="../../assets/Mine/相机.png" alt="">
-                            <span>身份证反面</span>
-                        </div>
-                    </div>
+               <div class='self'>                
+                <el-upload
+                    class="avatar-uploader"
+                    action="http://cloud.eyun.online:9080/file/api/ossUpload"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+                <el-upload
+                    class="avatar-uploader"
+                    action="http://cloud.eyun.online:9080/file/api/ossUpload"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess2"
+                    :before-upload="beforeAvatarUpload2">
+                    <img v-if="imageUrl2" :src="imageUrl2" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
                </div>
                 
             </div>
 
             <div class='main'>
                <h2>本人手持身份证照片</h2>
-                <div class='list'>
-                        <input type="file"  class='file'>
-                         <div class='relative'>
-                            <img src="../../assets/Mine/相机.png" alt="">
-                            <span>上传照片</span>
-                        </div>
-                </div>
+                <el-upload
+                    class="avatar-uploader"
+                    action="http://cloud.eyun.online:9080/file/api/ossUpload"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess3"
+                    :before-upload="beforeAvatarUpload3">
+                    <img v-if="imageUrl3" :src="imageUrl3" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+               <p class="photo" style="display:none">
+                   <span class="front">{{idcardfront}}</span>
+                   <span class="back">{{idcardback}}</span>
+                   <span class="hold">{{idcardhold}}</span>
+                   <span class="license">{{License}}</span>
+                   <span class="shopname">{{shopname}}</span>
+                </p>      
             </div>
 
             <div class='main'>
                <h2>企业营业执照照片</h2>
-                 <div class='list'>
-                        <input type="file"  class='file'>
-                         <div class='relative'>
-                            <img src="../../assets/Mine/相机.png" alt="">
-                            <span>上传照片</span>
-                        </div>
-                 </div>
+                 <el-upload
+                    class="avatar-uploader"
+                    action="http://cloud.eyun.online:9080/file/api/ossUpload"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess4"
+                    :before-upload="beforeAvatarUpload4">
+                    <img v-if="imageUrl4" :src="imageUrl4" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
             </div>
-
-            <div class='store'>
-                <h2>店铺名称</h2>
-                <input type="text" placeholder="请输入店铺的名称">
-                <button>提交申请</button>
+            <div class='store'>               
+                <h2>店铺名称</h2>            
+                <input type="text" placeholder="请输入店铺的名称" class="shpname">
+                <button class="act">提交申请</button>
             </div>
         </div>
    </div>
 </template>
 
 <script>
-import { Header } from 'mint-ui';
+import axios from 'axios'
+import { Header } from "mint-ui";
+import { setCookie,getCookie } from '../../assets/js/cookie.js';
 export default {
-        methods:{
-            
-        }
-}
+  data() {
+    return {
+      imageUrl: "",
+      imageUrl2: "",
+      imageUrl3: "",
+      imageUrl4: "",
+      idcardfront: "",
+      idcardback: "",
+      idcardhold: "",
+      License: "",
+      shopname: ""
+    };
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+      console.log(res[0]);
+      this.idcardfront = res[0];
+    },
+    handleAvatarSuccess2(res, file) {
+      this.imageUrl2 = URL.createObjectURL(file.raw);
+      console.log(res);
+      this.idcardback = res[0];
+    },
+    handleAvatarSuccess3(res, file) {
+      this.imageUrl3 = URL.createObjectURL(file.raw);
+      console.log(res);
+      this.idcardhold = res[0];
+    },
+    handleAvatarSuccess4(res, file) {
+      this.imageUrl4 = URL.createObjectURL(file.raw);
+      console.log(res);
+      this.License = res[0];
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    beforeAvatarUpload2(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    beforeAvatarUpload3(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    beforeAvatarUpload4(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    }
+  },
+  mounted: function() {
+
+  var accessToken = getCookie('access_token');
+    $(".act").click(function() {
+      var front = $(".front").text();
+      var back = $(".back").text();
+      var hold = $(".hold").text();
+      var license = $(".license").text();
+      var shopname = $(".shpname").val();
+      var data = {
+        imgIdcardFront: front,
+        imgIdcardBack: back,
+        imgIdcardHold: hold,
+        imgLicense: license,
+        name: shopname
+      };
+      console.log(data);
+
+    $.ajax({
+          url: "http://cloud.eyun.online:9080/user/api/mercuries/uploadMercuryImages",
+          // method:'post',
+          type: "POST",
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify(data),
+           headers:{
+                  'Content-Type': 'application/json',
+                  Authorization: "Bearer " + accessToken
+                },
+          success: function(res) {
+            console.log(res);
+            alert("上传成功，等待审核")
+            //top
+          },
+          error(res) {
+            console.log(res);
+          }
+        });
+
+    });
+
+   
+  }
+};
 </script>
 
 <style scoped>
-.OrdinaryBusiness{
-    width: 100%;
-    height: 100%; 
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    overflow: hidden;
-    background:#f5f5f5;
+.OrdinaryBusiness {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+  background: #f5f5f5;
 }
-header{
-    width:100%;
-    background:#fff;
-    color:#2f2f2f;
-    height:.94rem;
-    font-size:.32rem;
-    border-bottom:1px solid #e7e7e7;
+header {
+  width: 100%;
+  background: #fff;
+  color: #2f2f2f;
+  height: 0.94rem;
+  font-size: 0.32rem;
+  border-bottom: 1px solid #e7e7e7;
 }
-p{
-    height:.78rem;
-    line-height: .78rem;
-    font-size:.22rem;
-    color:#696969;
-    padding-left:.3rem;
+p {
+  height: 0.78rem;
+  line-height: 0.78rem;
+  font-size: 0.22rem;
+  color: #696969;
+  padding-left: 0.3rem;
 }
-.main{
-    background:#fff;
-    margin-top:.12rem;
-    width:100%;
+.main {
+  background: #fff;
+  margin-top: 0.12rem;
+  width: 100%;
+  padding-bottom: 0.4rem;
 }
-h2{
-    font-size:.28rem;
-    color:#2f2f2f;
-    padding-left:.3rem;
-    padding-top:.2rem;
+h2 {
+  font-size: 0.28rem;
+  color: #2f2f2f;
+  padding-left: 0.3rem;
+  padding-top: 0.2rem;
 }
-.main .self{
-    display:flex;
+.main .self {
+  display: flex;
 }
-.main .self .list:nth-child(2){
-    margin-left:.4rem;
+.main .self .list:nth-child(2) {
+  margin-left: 0.4rem;
 }
-.list{
-    position: relative;
-    padding-top:.4rem;
-    padding-left:.3rem;
-    padding-bottom:.32rem; 
+.list {
+  position: relative;
+  padding-top: 0.4rem;
+  padding-left: 0.3rem;
+  padding-bottom: 0.32rem;
 }
-.relative{
-    position: absolute;
-    top:.39rem;
-    width:2.4rem;
-    height:1.6rem;
-    border:1px dashed #c4c4c4;
+.relative {
+  position: absolute;
+  top: 0.39rem;
+  width: 2.4rem;
+  height: 1.6rem;
+  border: 1px dashed #c4c4c4;
 }
-.relative img{
-    width:.78rem;
-    height:.7rem;
-    margin-left:.8rem;
-    margin-top:.24rem;
-    margin-bottom:.24rem;
-    display:block;
+.relative img {
+  width: 0.78rem;
+  height: 0.7rem;
+  margin-left: 0.8rem;
+  margin-top: 0.24rem;
+  margin-bottom: 0.24rem;
+  display: block;
 }
-.relative span{
-    display:block;
-    text-align:center;
-    font-size:.24rem;
-    color:#c4c4c4;
+.relative span {
+  display: block;
+  text-align: center;
+  font-size: 0.24rem;
+  color: #c4c4c4;
 }
-.file{
-    width:2.4rem;
-    height:1.6rem;
-    position: relative;
-    z-index:22;
-    opacity:0;
+.file {
+  width: 2.4rem;
+  height: 1.6rem;
+  position: relative;
+  z-index: 22;
+  opacity: 0;
 }
-.store{
-    background:#fff;
-    margin-top:.12rem;
-    height:3.03rem;
+.store {
+  background: #fff;
+  margin-top: 0.12rem;
+  height: 3.03rem;
 }
-.store input{
-    height:.54rem;
-    width:70%;
-    border-radius: .11rem;
-    background:#f5f5f5;
-    border:0;
-    margin-left:.3rem;
-    margin-top:.24rem;
-    padding-left:.2rem;   
-    font-size:.28rem;
+.store input {
+  height: 0.54rem;
+  width: 70%;
+  border-radius: 0.11rem;
+  background: #f5f5f5;
+  border: 0;
+  margin-left: 0.3rem;
+  margin-top: 0.24rem;
+  padding-left: 0.2rem;
+  font-size: 0.28rem;
 }
-input::-webkit-input-placeholder{ 
- color:#b7b7b7;
+input::-webkit-input-placeholder {
+  color: #b7b7b7;
 }
-.store button{
-    display:block;
-    width:70%;
-    margin-left:15%;
-    margin-top:.6rem;
-    height:.8rem;
-    border-radius:.12rem;
-    background: #0bacff;
-    border:0;
-    font-size:.32rem;
-    color:#fffefe;
+.store button {
+  display: block;
+  width: 70%;
+  margin-left: 15%;
+  margin-top: 0.6rem;
+  height: 0.8rem;
+  border-radius: 0.12rem;
+  background: #0bacff;
+  border: 0;
+  font-size: 0.32rem;
+  color: #fffefe;
+}
+.avatar-uploader {
+  /* background: red; */
+  width: 1.78rem;
+  height: 1.78rem;
+  border: 1px dashed #d9d9d9;
+  margin-left: 0.4rem;
+  margin-top: 0.4rem;
+}
+
+.avatar-uploader .el-upload {
+  border: 1px solid #d9d9d9;
+  border-radius: 0.2rem;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 1.78rem;
+  height: 1.78rem;
+  line-height: 1.78rem;
+  text-align: center;
+}
+.avatar {
+  width: 1.78rem;
+  height: 1.78rem;
+  display: block;
 }
 </style>
