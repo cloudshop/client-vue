47bc7f413599778dbbf52689e54ca65618549d72
unknown <13691226692@163.com>
2018-04-28 14:27:36 +0800

    重置密码

--- src/components/Login/RegisterNew.vue
+++ src/components/Login/RegisterNew.vue
@@ -3,32 +3,33 @@
       <header class="mint-header">
            <div class="mint-header-button is-left">
                <a class="router-link-active">
-                  <router-link :to="{ path: '/Login' }" class="mint-button mint-button--default mint-button--normal" tag='button'>
-                   <mt-button icon="back"></mt-button>
-                  </router-link>
+                  <mt-button icon="back" @click="callBack"></mt-button>
               </a>
             </div> 
              <h1 class="mint-header-title">重置手机密码</h1>
             <div class="mint-header-button is-right"></div>
      </header>
-    <div class="register_main">
-
+    <div class="register_main" v-show="flag">
       <div class="iphone">
         <p>+86 <span>∨</span></p>
-        <input type="text" placeholder="请输入手机号" id="mytest"  v-model="phone">
-        <div class="iphones"><span class="one">|</span><span class="iphone_btn" @click='gain'>获取验证码</span></div>
+        <input type="text" placeholder="请输入手机号" id="mytest" class="iphones"  v-model="phone" @blur="upperCase">
+        <div class="iphones"><span class="one">|</span><button class="iphone_btn"  id='iphone_btn' @click='gain(".iphone_btn")'>获取验证码</button></div>
       </div>
-
       <div class="iphone">
         <p class="authCode"><img src="../../assets/Login/验证码-(1).png" alt=""></p>
-        <input type="text" placeholder="请输入验证码"  v-model="authCode">
+        <input type="text" id="yzms" placeholder="请输入验证码"  v-model="authCode">
       </div>
-
       <div class="next">      
-        <input type='button' class="next_btn" @click="next" value='下一步' >
+        <input type='button' class="next_btn" @click="nextPwd" value='下一步' >
       </div>
-
     </div>
+    <div class="center content" v-show="flag1">
+        <p>设置密码<input type="password" class="input" id="setPassword" v-model="setPassword" ></p>
+        <p class="yzm">确认密码<input type="password" class="input" id="affirmPassword" v-model="affirmPassword"></p>
+          <div class="nexts">      
+            <input type='button' class="nexts_btn" @click="next" value='下一步' >
+          </div>
+      </div>
   </div>
 </template>
 
@@ -39,70 +40,168 @@ export default {
       return{
         phone:'',
         authCode:'',
-        recommend: '',
+        setPassword:'',
+        affirmPassword: '',
+        flag: true,
+        flag1: false
       }
     },
     methods:{
-      next(){
-          // var recommends=document.getElementById("recommends").value; 
-          // var p1=/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
-          // setCookie('authCode',this.authCode,1000*60)
-          // //(p1.test(theinput)); 
-          // var that = this;
-          // if(p1.test(recommends) != "") { 
-          //   if(p1.test(recommends)==false) { 
-          //     alert('请填写推荐人正确手机号！'); 
-          //     document.getElementById("mytest").value="";
-          //   }
-          // }else {
-          //   console.log(0)
-          //   $.ajax({
-          //     url:'http://cloud.eyun.online:9080/verify/api/verify/smsvalidate?'+'phone='+this.phone+'&smsCode='+this.authCode,
-          //     method:'get',
-          //     callback:'cb',
-          //     success:function(res){
-          //       if(res.message == 'success'){
-          //         that.$router.push({path:'/SetPsd'})
-          //       }else{
-          //         alert('验证码填写错误')
-          //       }
-          //     },  
-          //     error(res){
-          //       console.log(res)
-          //     }
-          //   })
-          // }
-          // this.$router.push({path:'/SetPsd'})
+        callBack(){
+          if(this.flag==false){
+            this.flag=true;
+            this.flag1=false;
+          }else{
+            this.$router.go(-1)
+          }
+        },
+       upperCase() {
+        var theinput = document.getElementsByClassName("iphones")[0].value;
+        var p1 = /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
+        // var p2 = /^[\^\\%@&\*~'\?\/\<\>\|\"`]+$/;
+        if (p1.test(theinput) == false) {
+          alert("请填写正确电话号码!!");
+          document.getElementsByClassName("iphones")[0].value = "";
+        } else {
+          this.iphoneYN = true;
+        }
       },
-      gain(){
-          // var theinput=document.getElementById("mytest").value; 
-          // setCookie('iphone',this.theinput,1000*60)
-          // var p1=/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/; 
-          // if(p1.test(theinput)==false) { 
-          //   alert('请填写正确手机号！'); 
-          //   document.getElementById("mytest").value="";
-          // }else {
-          //   $.ajax({
-          //     url:'http://cloud.eyun.online:9080/verify/api/verify/smscode?phone='+this.phone,
-          //     method:'get',
-          //     callback:'cb',
-          //     success:function(res){
-          //       console.log(res)
-          //     },  
-          //     error(res){
-          //       console.log(res)
-          //     }
-          //   })
-          // }
+      nextPwd:function(){
+        this.flag=false;
+        this.flag1=true;
+      },
+      next(){
+         var setPassword=document.getElementById("setPassword").value; 
+         var affirmPassword=document.getElementById("affirmPassword").value; 
+         var p1=/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
+          var that = this;
+          if(setPassword !== affirmPassword) { 
+            alert('两次密码输入不符')
+            document.getElementById("setPassword").value="";
+            document.getElementById("affirmPassword").value="";
+          }else {
+            var data = {'login':this.phone,'password':this.setPassword,'verifyCode':this.authCode}
+            this.$axios.post('http://cloud.eyun.online:9080/uaa/api/account/update-password',data)
+            .then(function(response) {
+              // setCookie('access_token',accessToken,1000*60);
+            })
+            .catch(function(error) {
+              if(error.response.status === 500){
+                  alert('服务器繁忙，请耐心等待')
+              }
+            });
+        }
+      },
+      gain(obj){
+          var theinput=document.getElementById("mytest").value; 
+          setCookie('iphone',this.theinput,1000*60)
+          var p1=/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/; 
+          if(p1.test(theinput)==false) { 
+            alert('请填写正确手机号！'); 
+            document.getElementById("mytest").value="";
+          }else {
+            this.$axios.get('http://cloud.eyun.online:9080/verify/api/verify/smscode/login/'+this.phone)
+            .then(function(response) {
+              var countdown=60;
+              settime(obj);
+              function settime(obj) {
+                  if (countdown == 0) {
+                      $(obj).attr("disabled",false);
+                      $(obj).text("获取验证码");
+                      countdown = 60;
+                      return;
+                  } else {
+                      $(obj).attr("disabled",true);
+                      $(obj).text(countdown + " s 重新发送");
+                      countdown--;
+                  }
+                    setTimeout(function() {
+                      settime(obj) 
+                    }
+                  ,1000)
+                }
+            })
+            .catch(function(error) {
+              if(error.response.status === 500){
+                  alert('服务器繁忙，请耐心等待')
+              }
+            });
+          }
       }
     },
     created(){
 
+    },
+    mounted:function () {
+        $('input').on('keyup',function(){
+            if($('#mytest').val().length>=1 && $('#yzms').val().length>=1){
+                $('.next_btn').addClass('Color')         
+            }else{
+                $('.next_btn').removeClass('Color')  
+            }
+        })
+        $('input').on('keyup',function(){
+            if($('#setPassword').val().length>=1 && $('#affirmPassword').val().length>=1){
+                $('.nexts_btn').addClass('Color')         
+            }else{
+                $('.nexts_btn').removeClass('Color')  
+            }
+        })
     }
 }
 </script>
 
 <style scoped>
+    .Color {
+      background: red !important;
+    }
+   .center{
+        width: 98%;
+        background: #fff;
+        font-size: .32rem;
+        margin-left: 2%;
+    }
+    .center .input{
+        width: 3rem;
+        margin-left: .3rem;
+        border: none;
+    }
+    .center p:first-child{
+        width: 98%;
+        margin: auto;
+        height: .95rem;
+        line-height: .95rem;
+        font-size: .32rem;
+        border-bottom: 1px solid #e7e7e7;
+    }
+    .yzm{
+        width: 98%;
+        margin: auto;
+        height: .95rem;
+        font-size: .32rem;
+        line-height: .95rem;
+        border-bottom: 1px solid #e7e7e7;
+    }
+    .yzm button{
+       height: .95rem; 
+       flex: 1;
+       border: none;
+       border: 1px solid #e7e7e7;
+       background: #fff;
+       border-left: none;
+       border-top-right-radius: .2rem;
+       border-bottom-right-radius: .2rem;
+       border-left: 1px solid #e7e7e7;
+       margin-left: -.3rem;
+    }
+    #iphone_btn{
+      height: 100%;
+      display: flex;
+      font-size: .26rem;
+      color: #2f2f2f;
+      border: none;
+      background: #fff;
+    }
 .registers{
   width: 100%;
   display: flex;
@@ -228,6 +327,23 @@ input::-webkit-input-placeholder{
 }
 .next_btn{
   width: 100%;
+  height: .96rem;
+  color:#fff;
+  font-size: .32rem;
+  text-align: center;
+  line-height: .96rem;
+  display: inline-block;
+  background: #c4c4c4;
+  border-radius: .08rem;
+  border:0;
+}
+.nexts{
+  width: 98%;
+  height: .96rem;
+  margin-top: .5rem;
+}
+.nexts_btn{
+  width: 98%;
   height: .96rem;
   color:#fff;
   font-size: .32rem;
