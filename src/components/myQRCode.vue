64005c772d5a8ad0e08bfe3fd2a73010eb81a23e
unknown <13691226692@163.com>
2018-04-28 14:54:51 +0800

    二维码

---
+++ src/components/myQRCode.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="myQrCode">
+        <div id="qrcode" v-show='flag' ref="qrcode"></div>
+        <div class="logins" v-show="flag1">
+            <p class="goLogin">您还未登陆，请先去登录</p>
+        </div>
+        <Foot></Foot>
+	</div>
+</template>
+
+<script> 
+import Foot from './main/Foot'
+export default {
+    data(){
+        return{
+            flag: true,
+            flag1: false,
+            i:false,
+            userInfo: ''
+        }
+    },
+    created(){
+        // console.log(document.cookie);
+        // if(accessToken == ''){
+        //     this.flag=false;
+        // }
+        // else{
+        //     this.flag1=true;
+        // }
+        this.userInfo= '小可耐'
+    },
+    mounted() {
+        this._getQart()
+    },
+
+    methods: {
+        _getQart: function() {
+            if(this.i==false){
+                var qrcode = new QRCode(document.getElementById("qrcode"), {  
+                    width : 200,//设置宽高  
+                    height : 200  
+                });  
+                qrcode.makeCode(this.userInfo);  
+            }
+            this.i = true;
+        },
+    },
+    components:{
+        Foot
+    },
+}
+</script>
+
+<style>
+	#qrcode{
+		height: 200px;
+		width: 200px;
+		margin: auto;
+	}
+</style>
