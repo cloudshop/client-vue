90a20a5dabab0e16becffb7caf2a6ef98c4b9448
unknown <13691226692@163.com>
2018-05-03 11:16:08 +0800

    setpsd

--- src/components/Login/SetPsd.vue
+++ src/components/Login/SetPsd.vue
@@ -48,36 +48,79 @@ export default {
             var flag = ''
             if(setPassword == affirmPassword){
                 if(iphone !== '' &&  setPassword !== '' && authCode !== ''){
-                    var that = this;
-                    var UaaApi = require('api-uaa');
-                    UaaApi.ApiClient.instance.basePath = '/api/uaa'
-                    var apiInstance = new UaaApi.AccountResourceApi();
-                    var managedUserVM = new UaaApi.ManagedUserVM(); // ManagedUserVM | managedUserVM
-                    managedUserVM.login = iphone
-                    managedUserVM.password = this.setPassword
-                    managedUserVM.verifyCode = authCode
-                    managedUserVM.inviterPhone = recommend
-                    var callback = function(error, data, response) {
-                        if (error) {
-                            if(response.body.title == "Verification code error, please re - enter!"){
-                                alert('验证码输入错误')
-                            }else{
-                                alert('网络已经飞快去请求啦，请不要重复操作')
-                            }
-                        } else {
-                            alert('注册成功')
-                            var  val={
-                                "func":"closeCurrent",
-                                "param":{'finallyIndex':'4','refreshAll':true},
-                                };
-                            var u = navigator.userAgent;
-                            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
-                            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-                            if(isiOS){
-                                window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
-                            }else if(isAndroid){  
-                                window.androidObject.JSCallAndroid(JSON.stringify(val));
-                            }
+                        var that = this;
+                        var UaaApi = require('api-uaa');
+                        UaaApi.ApiClient.instance.basePath = '/api/uaa'
+                        var apiInstance = new UaaApi.AccountResourceApi();
+                        var managedUserVM = new UaaApi.ManagedUserVM(); // ManagedUserVM | managedUserVM
+                        managedUserVM.login = iphone
+                        managedUserVM.password = this.setPassword
+                        managedUserVM.verifyCode = authCode
+                        managedUserVM.inviterPhone = recommend
+                        var callback = function(error, data, response) {
+                            if (error) {
+                                // alert(error)
+                                if(error.response.error.status == 404){
+                                    alert('验证码输入错误')
+                                }
+                            } else {
+                                console.log('API called successfully.');
+                                var credentials = {
+                                client: {
+                                    id: "web_app",
+                                    secret: "w1eb_app"
+                                },
+                                auth: {
+                                    tokenHost: "api",
+                                    tokenPath: "/auth/login/app"
+                                },
+                                http: {
+                                    headers: {
+                                        Accept: "application/json"
+                                    }
+                                },
+                                options: {
+                                    bodyFormat: "json"
+                                }
+                            };
+                            const oauth2 = require("simple-oauth2").create(credentials);
+                            const tokenConfig = {
+                                username: iphone,
+                                password: that.setPassword
+                            };
+                            var token;
+                            oauth2.ownerPassword.getToken(tokenConfig).then(result => {
+                            const accessToken = oauth2.accessToken.create(result);
+                            token = accessToken.token.access_token;
+                            var ApiVerify = require("api-uaa");
+                            ApiVerify.ApiClient.instance.basePath = "/api/uaa";
+                            ApiVerify.ApiClient.instance.defaultHeaders = {
+                                Authorization: "Bearer " + token
+                            };
+                            var api = new ApiVerify.AccountResourceApi();
+                            var callback = function(error, data, response) {
+                                if (error) {
+                                 alert('您已注册成功，请去登录');
+                                } else {
+                                    alert('注册成功')
+                                    setCookie('login',1)
+                                    var  val={
+                                        "func":"closeCurrent",
+                                        "param":{'finallyIndex':'4','refreshAll':true},
+                                        };
+                                    var u = navigator.userAgent;
+                                    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
+                                    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+                                    if(isiOS){
+                                        window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+                                    }else if(isAndroid){  
+                                        window.androidObject.JSCallAndroid(JSON.stringify(val));
+                                    }
+                                }
+                            };
+                            api.getAccountUsingGET(callback);
+                            return accessToken;
+                            });
                         }
                     }
                     apiInstance.registerAppAccountUsingPOST(managedUserVM, callback);
