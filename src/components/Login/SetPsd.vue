90c58a0907247bd45a8d7139ea193d930a094679
unknown <13691226692@163.com>
2018-05-03 16:38:04 +0800

    ser

--- src/components/Login/SetPsd.vue
+++ src/components/Login/SetPsd.vue
@@ -44,45 +44,53 @@ export default {
                 verifyCode : authCode,
                 inviterPhone : recommend
             }
-            var data = {'username':iphone,'password':this.setPassword}
             var flag = ''
             if(setPassword == affirmPassword){
                 if(iphone !== '' &&  setPassword !== '' && authCode !== ''){
-                    var that = this;
-                    const data = {
-                        username: iphone,
-                        password: that.setPassword
-                    };
-                    console.log(data)
-                    var ApiVerify = require("api-uaa");
-                    ApiVerify.ApiClient.instance.basePath = "/api/uaa";
-                    var api = new ApiVerify.AccountResourceApi();
-                    console.log(api)
-                    var callback = function(error, data, response) {
-                        console.log(error)
-                        console.log(data)
+                    this.$axios.post('api/uaa/api/register/app',val)
+                    .then(function(response) {
                         console.log(response)
-                        if (error) {
-                            alert('您已注册成功登录error');
-                        } else {
-                            alert('注册成功')
-                            setCookie('login',1)
-                            console.log('注册成功')
-                            var  val={
-                                "func":"closeCurrent",
-                                "param":{'finallyIndex':'4','refreshAll':true},
-                                };
-                            var u = navigator.userAgent;
-                            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
-                            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-                            if(isiOS){
-                                window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
-                            }else if(isAndroid){  
-                                window.androidObject.JSCallAndroid(JSON.stringify(val));
-                            }
-                        }
-                    };
-                    api.getAccountUsingGET(callback);
+                        console.log(response.data)
+                        alert('注册成功')
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    }); 
+                    // var that = this;
+                    // const data = {
+                    //     username: iphone,
+                    //     password: that.setPassword
+                    // };
+                    // console.log(data)
+                    // var ApiVerify = require("api-uaa");
+                    // ApiVerify.ApiClient.instance.basePath = "/api/uaa";
+                    // var api = new ApiVerify.AccountResourceApi();
+                    // console.log(api)
+                    // var callback = function(error, data, response) {
+                    //     console.log(error)
+                    //     console.log(data)
+                    //     console.log(response)
+                    //     if (error) {
+                    //         alert('您已注册成功登录error');
+                    //     } else {
+                    //         alert('注册成功')
+                    //         setCookie('login',1)
+                    //         console.log('注册成功')
+                    //         var  val={
+                    //             "func":"closeCurrent",
+                    //             "param":{'finallyIndex':'4','refreshAll':true},
+                    //             };
+                    //         var u = navigator.userAgent;
+                    //         var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
+                    //         var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+                    //         if(isiOS){
+                    //             window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+                    //         }else if(isAndroid){  
+                    //             window.androidObject.JSCallAndroid(JSON.stringify(val));
+                    //         }
+                    //     }
+                    // };
+                    // api.getAccountUsingGET(callback);
                 }else{
                     alert('请确定是否输入手机号、验证码、密码')
                 }
