62d34bb930163e5261d82636f1020ba8c489f844
unknown <13691226692@163.com>
2018-05-07 11:15:49 +0800

    二维码

--- src/components/myQRCode.vue
+++ src/components/myQRCode.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="myQrCode">
+        <div v-show='false' class="phones">{{phones}}</div>
         <div id="qrcode" v-show='flag' ref="qrcode"></div>
         <div class="logins" v-show="flag1">
             <p class="goLogin">您还未登陆，请先去登录</p>
@@ -9,30 +10,40 @@
 </template>
 
 <script> 
-import Foot from './main/Foot'
+import { setCookie, getCookie } from "../assets/js/cookie.js";
+import qrcode from "../../static/js/qrcode.js";
+import Foot from './main/Foot';
+
 export default {
     data(){
         return{
             flag: true,
             flag1: false,
             i:false,
-            userInfo: ''
+            userInfoTwo: '',
+            phones:''
         }
     },
     created(){
-        // console.log(document.cookie);
-        // if(accessToken == ''){
-        //     this.flag=false;
-        // }
-        // else{
-        //     this.flag1=true;
-        // }
-        this.userInfo= '小可耐'
+        var Cookie = getCookie('login');
+        var phoness = getCookie('PSTM');
+        this.$axios.get("api/user/api/user-annexes/userInfo")
+        .then((res) => {
+            this.phones = res.data.phone
+        })
+        var phone = $(".phones").text()
+        if(Cookie == ''){
+            this.userInfoTwo= 'http://m.anzhi.com/app_b222_com.grjf365.gongrongpoints.html';
+        }
+        else{
+            this.userInfoTwo= 'http://app.grjf365.com/simpleregister/index.html?phoneNumber'+'='+phone
+        }
     },
+    
     mounted() {
         this._getQart()
+        this._phones()
     },
-
     methods: {
         _getQart: function() {
             if(this.i==false){
@@ -40,9 +51,13 @@ export default {
                     width : 200,//设置宽高  
                     height : 200  
                 });  
-                qrcode.makeCode(this.userInfo);  
+                qrcode.makeCode(this.userInfoTwo);  
             }
             this.i = true;
+        },
+        _phones:function(){
+            this.phones = 11111
+           
         },
     },
     components:{
