52976e04154603f2007a11d82735eadb6af234a1
Bob Ying <124599792@qq.com>
2018-05-09 10:57:08 +0800

    login error catch

--- src/store/store.js
+++ src/store/store.js
@@ -106,44 +106,38 @@ export default new Vuex.Store({
                 registrationID: userInput.registrationID
             }
 
-            // Promises
-            // Save the access token
-            oauth2.ownerPassword.getToken(tokenConfig)
-                .then((result) => {
-                    console.log('Access Token 1', result);
-                    const accessToken = oauth2.accessToken.create(result)
-                    console.log('Access Token 2', accessToken);
-                    // store the token in global variable ??
-                    context.commit(types.LOGIN, accessToken);
-
-                    var val = {
-                        "func": "closeCurrent",
-                        "param": {
-                            'finallyIndex': '1',
-                            'refreshAll': true
-                        },
-                    };
-                    var u = navigator.userAgent;
-                    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
-                    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-                    if (isiOS) {
-                        window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
-                    } else if (isAndroid) {
-                        window.androidObject.JSCallAndroid(JSON.stringify(val));
-                    }
-                    return true;
-                })
-                .catch((error) => {
-                    console.log('Access Token Error', error.message);
-
+            oauth2.ownerPassword.getToken(tokenConfig, (error, result) => {
+                if (error) {
                     if (error.response.status === 500) {
                         alert('服务器繁忙，请耐心等待')
                     }
                     if (error.response.status === 400) {
                         alert('用户名密码错误')
                     }
-                    console.log('Access Token error', error.message);
-                })
+                    return console.log('Access Token Error', error.message);
+                }
+
+                console.log('Access Token 1', result);
+                const accessToken = oauth2.accessToken.create(result);
+                console.log('Access Token 2', accessToken);
+                context.commit(types.LOGIN, accessToken);
+
+                var val = {
+                    "func": "closeCurrent",
+                    "param": {
+                        'finallyIndex': '1',
+                        'refreshAll': true
+                    },
+                };
+                var u = navigator.userAgent;
+                var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
+                var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+                if (isiOS) {
+                    window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+                } else if (isAndroid) {
+                    window.androidObject.JSCallAndroid(JSON.stringify(val));
+                }
+            });
         },
 
         refresh: function (context) {
