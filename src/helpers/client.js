a5efb77ed08f9173fda48f0cacc4c7223294801e
Bob Ying <124599792@qq.com>
2018-05-09 13:19:39 +0800

    拦截器使用store正常

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -5,7 +5,9 @@
 
 import axios from 'axios'
 import * as types from '../store/types'
+import store from '../store/store'
 import router from '../router'
+import storePlugin from '../storePlugin'
 
 // axios 配置
 axios.defaults.timeout = 5000;
@@ -14,36 +16,40 @@ axios.defaults.baseURL = 'http://app.grjf365.com:9080/';
 // http request 拦截器
 axios.interceptors.request.use(
     config => {
-        if (this.$store.getters.bearToken !== null) {
-            console.log("add token")
-           config.headers.Authorization = this.$store.getters.bearToken
+        console.log("add token")
+        console.log(store)
+        console.log(store.getters)
+        console.log(store.getters.bearToken)
+        if (store.getters.bearToken !== null) {
+            console.log("add token 111")
+            config.headers.Authorization = store.getters.bearToken
         }
         return config;
     },
     err => {
-         console.log("add error")
+        console.log("add error")
         return Promise.reject(err);
     });
 
 // http response 拦截器
-axios.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        if (error.response) {
-            switch (error.response.status) {
-                case 401:
-                    // 401 清除token信息并跳转到登录页面
-                    this.$store.commit(types.LOGOUT);
-                    router.replace({
-                        path: 'login',
-                        query: {redirect: router.currentRoute.fullPath}
-                    })
-            }
-        }
-        console.log(JSON.stringify(error));//console : Error: Request failed with status code 402
-        return Promise.reject(err);
-    });
+// axios.interceptors.response.use(
+//     response => {
+//         return response;
+//     },
+//     error => {
+//         if (error.response) {
+//             switch (error.response.status) {
+//                 case 401:
+//                     // 401 清除token信息并跳转到登录页面
+//                     this.$store.commit(types.LOGOUT);
+//                     router.replace({
+//                         path: 'login',
+//                         query: {redirect: router.currentRoute.fullPath}
+//                     })
+//             }
+//         }
+//         console.log(JSON.stringify(error));//console : Error: Request failed with status code 402
+//         return Promise.reject(err);
+//     });
 
 export default axios;
