5a78928c4c92164c98fa319d4eca0fa894d8e25c
Bob Ying <124599792@qq.com>
2018-05-11 19:44:38 +0800

    test of access token on vue with IOS

--- src/helpers/storage-native.js
+++
@@ -1,8 +0,0 @@
-function dumpLocalStorageToJSON() {
-    d = {};
-    for(i = 0; i < localStorage.length; i++)
-      d[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
-    return JSON.stringify(d);
-}
-
-export default dumpLocalStorageToJSON;
