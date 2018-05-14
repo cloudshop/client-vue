b4373c5e10f06a22ab02fb3ec82485d9d6c828d4
qi jianbei <17600045817@163.com>
2018-05-14 12:04:26 +0000

    qijianbei  Update RemainingSum.vue

--- src/components/MineList/RemainingSum.vue
+++ src/components/MineList/RemainingSum.vue
@@ -16,12 +16,15 @@
                   <p>余额</p>
                   <h2>{{arr.balance}}</h2>
           </div>
-          <!--<ul class='list'>-->
-          <!--  <li>-->
-          <!--    <p><span>2018-03-06</span><span>周二</span></p>-->
-          <!--    <p><b>提现</b><b>-1000</b></p>-->
-          <!--  </li>-->
-          <!--</ul>-->
+          <div class="main">
+            <ul class='list' v-for="item in brr">
+            <li>
+              <!-- <p><span>{{item.createdTime}}</span><span></span></p> -->
+              <p><span>{{item.createdTime}}</span><span></span></p>
+              <p><b>{{item.typeString}}</b><b>{{item.balance}}</b></p>
+            </li>
+          </ul>
+          </div>
         </div>
 
         <footer class='footer'>
@@ -33,126 +36,139 @@
 </template>
 
 <script>
-import TouchBalance from './TouchBalance';
+import TouchBalance from "./TouchBalance";
 export default {
-      data(){
-          return{  
-              arr:'null'
-          }
-          
-      },
-   
-      created:{
-            
-      },
-      created(){
-             var that = this
-        this.$axios.get('wallet/api/wallets/user',{
-        })
-        .then(function(res){
-      //     console.log(res.data.balance)
-          that.arr = res.data
-      //      that.arr = res.data
-      //      console.log(arr)
-        })
-        .catch(function(error){
-          console.log(error)
-        })
-     },
-      methods:{
-            one:function(){
-                  
-            }
-      },
-}
+  data() {
+    return {
+      arr: "null",
+      brr: "null",
+      time:[],
+    };
+  },
+
+  created: {},
+  created() {
+    var that = this;
+    this.$axios
+      .get("wallet/api/wallets/user")
+      .then(function(res) {
+        that.arr = res.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    this.$axios
+      .get(
+        "wallet/api/wallet/details/balance?page=0&size=10&sort=createdTime%2Cdesc"
+      )
+      .then(function(res) {
+        that.brr = res.data;
+        console.log(that.brr)
+        that.brr = that.brr.filter((item)=>{
+          // return item.createdTime=item.createdTime.replace(/[A-Z]*/g, "");
+         return item.createdTime=item.createdTime.substr(0,19).replace('T', '    ');
+        }) 
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+  methods: {
+    one: function() {}
+  }
+};
 </script>
 
 <style scoped>
-header{
-    width:100%;
-    background:#fff;
-    color:#2f2f2f;
-    height:.94rem;
-    font-size:.32rem;
+header {
+  width: 100%;
+  background: #fff;
+  color: #2f2f2f;
+  height: 0.94rem;
+  font-size: 0.32rem;
 }
-.is-right{
-  color:#0096ff;
-  font-size:.28rem;
+.is-right {
+  color: #0096ff;
+  font-size: 0.28rem;
 }
-.RemainingSum{
-    width: 100%;
-    height: 100%; 
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    overflow: hidden;
-    background:#f5f5f5;
+.RemainingSum {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+  background: #f5f5f5;
 }
-.banner{
-  background:url("../../assets/Mine/RemainingSum/bg.png");
-  background-size:100%; 
-  width:100%;
-  height: 3.7rem;;
+.main{
+  padding-top: 3.7rem
 }
-.banner p{
-  text-align:center;
-  font-size:.32rem;
-  color:#fff;
-  padding-top:.98rem;
+.banner {
+  background: url("../../assets/Mine/RemainingSum/bg.png");
+  background-size: 100%;
+  width: 100%;
+  height: 3.7rem;
+  position: fixed;
+  top: .94rem
 }
-.banner h2{
-  text-align:center;
-  font-size:.8rem;
-  color:#fff;
-  padding-top:.36rem;
+.banner p {
+  text-align: center;
+  font-size: 0.32rem;
+  color: #fff;
+  padding-top: 0.98rem;
 }
-.list li{
-  height:1.44rem;
-  border-bottom:1px solid #dfdfdd;
+.banner h2 {
+  text-align: center;
+  font-size: 0.8rem;
+  color: #fff;
+  padding-top: 0.36rem;
 }
-.list li span{
-  color:#929292;
-  font-size:.22rem;
+.list li {
+  height: 1.44rem;
+  border-bottom: 1px solid #dfdfdd;
 }
-.list li p:nth-child(1){
-  padding-top:.37rem;
-  margin-left:.3rem;
+.list li span {
+  color: #929292;
+  font-size: 0.22rem;
 }
-.list li p:nth-child(1) span:nth-child(2){
-  padding-left:.3rem;
+.list li p:nth-child(1) {
+  padding-top: 0.37rem;
+  margin-left: 0.3rem;
 }
-.list li p:nth-child(2){
-  padding:0 .3rem;
-  display:flex;
-  justify-content:space-between;
-  margin-top:.16rem;
-
+.list li p:nth-child(1) span:nth-child(2) {
+  padding-left: 0.3rem;
 }
-.list li p:nth-child(2) b:nth-child(1){
-  font-size: .28rem;
-  color:#696969;
+.list li p:nth-child(2) {
+  padding: 0 0.3rem;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0.16rem;
 }
-.list li p:nth-child(2) b:nth-child(2){
-  font-size: .32rem;
-  color:#2f2f2f;
+.list li p:nth-child(2) b:nth-child(1) {
+  font-size: 0.28rem;
+  color: #696969;
 }
-.footer{
-  height:.98rem;
-  display:flex;
+.list li p:nth-child(2) b:nth-child(2) {
+  font-size: 0.32rem;
+  color: #2f2f2f;
 }
-.footer button{
-  width:50%;
-  font-size:.32rem;
-  border:0;
+.footer {
+  height: 0.98rem;
+  display: flex;
 }
-.footer button:first-child{
-  color:#1692e1;
-  background:#fff;
+.footer button {
+  width: 50%;
+  font-size: 0.32rem;
+  border: 0;
 }
-.footer button:last-child{
-  color:#fff;
-  background:#1692e1;
+.footer button:first-child {
+  color: #1692e1;
+  background: #fff;
+}
+.footer button:last-child {
+  color: #fff;
+  background: #1692e1;
 }
 </style>
