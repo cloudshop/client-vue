a1e34bb951f5ffea3e58d2515a7e80fb90a81935
zhangwentao <17810234576@163.com>
2018-05-15 18:46:55 +0800

    修改昵称

--- src/components/MineList/NickName.vue
+++ src/components/MineList/NickName.vue
@@ -1,4 +1,4 @@
-﻿<template>
+<template>
     <div>
         <header class="mint-header">
            <div class="mint-header-button is-left">
@@ -13,17 +13,19 @@
         </header>
        <mt-field placeholder="请输入昵称" v-model="name" id="nickname"></mt-field>
        <p class='hint'>4-10个字符,可由中英文,数字组成。</p>
-       <div class="msg" v-show="bol">
+       <div class="q" v-show="bol">
+         <div class="msg">
          <h4>提示：</h4>
           <p>修改成功</p>
           <router-link :to="{path:'/Information'}">确定</router-link>
        </div>
     </div>
+       </div>
 </template>
 
 <script>
 import { Header, Field } from "mint-ui";
-import axios from 'axios'
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -36,8 +38,11 @@ export default {
   },
   mounted: function() {
     var that = this;
+    // var bol = false;
     console.log(that.bol)
     $("#sure").click(function() {
+      // this.bol = true;
+      // console.log(this.bol)
       var nickname = $(".mint-field-core").val();
       var data = {
         nickname: nickname,
@@ -64,7 +69,7 @@ export default {
   },
   created(){
     this.Nickname = this.$route.params.nickname;
-    // console.log(this.Nickname);
+// console.log(this.Nickname);
     this.name = this.Nickname;
   }
 };
@@ -92,16 +97,26 @@ header {
   margin-top: 0.2rem;
   margin-left: 0.3rem;
 }
+.q{
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 5;
+  background: rgba(0,0,0,0.2);
+}
 .msg{
   width: 80%;
   height: 2.1rem;
-  position: relative;
+  position: absolute;
   left: 5%;
-  top: 30%;
+  top: 40%;
   z-index: 10;
   padding: 0 5%;
   background: #fff;
   border: 1px solid #eee;
+  border-radius: 0.15rem;
 }
 .msg h4{
   font-size: 0.3rem;
@@ -111,6 +126,7 @@ header {
 .msg p{
   height: 0.6rem;
   font-size: 0.24rem;
+  font-size: 0.25rem;
 }
 .msg a{
   display: block;
