3b7b56b2b7afc5ef6b063cd7ee4a94b9cf8b7627
zhangwentao <17810234576@163.com>
2018-05-15 10:33:31 +0800

    修改昵称

--- src/components/MineList/NickName.vue
+++ src/components/MineList/NickName.vue
@@ -9,34 +9,40 @@
               </a>
             </div> 
              <h1 class="mint-header-title">昵称设置</h1>
-            <div class="mint-header-button is-right" id="sure" @click="determine()">确认</div>
+            <div class="mint-header-button is-right" id="sure">确认</div>
         </header>
        <mt-field placeholder="请输入昵称" v-model="name" id="nickname"></mt-field>
        <p class='hint'>4-10个字符,可由中英文,数字组成。</p>
+       <div class="msg" v-show="bol">
+         <h4>提示：</h4>
+          <p>修改成功</p>
+          <router-link :to="{path:'/Information'}">确定</router-link>
+       </div>
     </div>
 </template>
 
 <script>
 import { Header, Field } from "mint-ui";
-import axios from 'axios';
+import axios from 'axios'
 export default {
   data() {
     return {
       name: "",
       sure: "",
-      popupVisible: true
+      popupVisible: true,
+      Nickname:"",
+      bol:false
     };
   },
   mounted: function() {
+    var that = this;
+    console.log(that.bol)
     $("#sure").click(function() {
       var nickname = $(".mint-field-core").val();
-      // var nickname = $('.mint-field-core').val()
-      //var data = { "avatar" :nickname,"type":2,"id":3 };
       var data = {
         nickname: nickname,
         type: 2
       };
-      // console.log(data);
       axios({
         method: "post",
         url:"user/api/user-annexes-useregis/updaUserInfo/",
@@ -44,7 +50,9 @@ export default {
       })
       .then(function(res) {
         console.log(res);
-        alert('修改成功')
+        //alert('修改成功')
+        that.bol = true;
+        console.log(that.bol)
       })
       .catch(function(error) {
         console.log(error);
@@ -52,11 +60,12 @@ export default {
     })
   },
   methods: {
-    // DataValidations() {
-    //   if (/^[\u4e00-\u9fa5_a-zA-Z0-9_]{4,10}$/.test(this.name)) {
-    //     alert("匹配正确");
-    //   }
-    // }
+    
+  },
+  created(){
+    this.Nickname = this.$route.params.nickname;
+    // console.log(this.Nickname);
+    this.name = this.Nickname;
   }
 };
 </script>
@@ -82,5 +91,35 @@ header {
   color: #c4c4c4;
   margin-top: 0.2rem;
   margin-left: 0.3rem;
+}
+.msg{
+  width: 80%;
+  height: 2.1rem;
+  position: relative;
+  left: 5%;
+  top: 30%;
+  z-index: 10;
+  padding: 0 5%;
+  background: #fff;
+  border: 1px solid #eee;
+}
+.msg h4{
+  font-size: 0.3rem;
+  line-break: 0.5rem;
+  line-height: 0.8rem;
+}
+.msg p{
+  height: 0.6rem;
+  font-size: 0.24rem;
+}
+.msg a{
+  display: block;
+  width: 20%;
+  height: 0.5rem; 
+  text-align: center;
+  line-height: 0.5rem;
+  color: #f00;
+  float: right;
+  font-size: 0.24rem;
 }
 </style>
