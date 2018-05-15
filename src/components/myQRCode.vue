51fb72ee2e1c83676076ecae545bc789ecb5d959
unknown <13691226692@163.com>
2018-05-15 18:46:15 +0800

    newMyQRCode

--- src/components/myQRCode.vue
+++ src/components/myQRCode.vue
@@ -1,9 +1,18 @@
 <template>
-	<div class="myQrCode">
-        <div id="qrcode" v-show='flag' ref="qrcode"></div>
-        <div class="logins" v-show="logined">您还未登录，请先去登录,否则无法分享哦~</div>
-        <Foot></Foot>
-	</div>
+       <div class="myQrCode">
+            <div class="myQrCode_header">
+                <p>〈</p >
+                <p>扫码注册</p >
+                <p></p>
+            </div>
+           <div class="myQrCode_body">
+                <div id="qrcode" v-show='flag' ref="qrcode"></div>
+                <div class="codeCenter"><img src="../assets/user/logoCode.png" alt=""></div>
+                <div class="borderCode"><img src="../assets/user/borderCode.png" alt=""></div>
+                <div class="logins" v-show="logined">您还未登录，请先去登录,否则无法分享哦~</div>
+           </div>
+           <Foot></Foot>
+       </div>
 </template>
 
 <script> 
@@ -34,8 +43,11 @@ export default {
                 }
                 if(this.i==false){
                     var qrcode = new QRCode(document.getElementById("qrcode"), {  
-                        width : 200,//设置宽高  
-                        height : 200  
+                        width : 170,//设置宽高  
+                        height : 170,
+                        colorDark : '#ff0000',
+                        colorLight : '#a3e6df',
+                        correctLevel : QRCode.CorrectLevel.H
                     });  
                     qrcode.makeCode(this.userInfoTwo);  
                 }
@@ -50,16 +62,72 @@ export default {
 </script>
 
 <style>
+    .myQrCode_header {
+        flex-shrink: 0;
+        width: 100%;
+        height: 0.96rem;
+        display: flex;
+        position: fixed;
+        top: 0;
+        z-index: 9;
+        justify-content: space-between;
+        align-items: center;
+        color: #2f2f2f;
+        background: #fff !important;
+        border-bottom: 1px solid #f8f8f8;
+    }
+    .myQrCode_header p:nth-child(1) {
+        padding-left: .4rem;
+    }
+    .myQrCode_header p:nth-child(2) {
+        font-size: 0.32rem;
+    }
+    .myQrCode{
+       position: relative;
+       background: red;
+    }
+    .myQrCode_body{
+        margin-top: .96rem;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background: url('../assets/user/background.png') !important;
+        background-repeat: no-repeat !important;  
+        background-size: 100% 100% !important;  
+    }
+    .codeCenter {
+        position: absolute;
+        width: 16%;
+        height: 9%;
+        bottom: 337px;
+        left: 152px;
+    }
+    .codeCenter img{
+        width: 100%;
+        height: 100%;
+    }
+    .borderCode{
+        position: absolute;
+        width: 54%;
+        height: 30%;
+        bottom: 267px;
+        left: 83px;
+    }
+    .borderCode img{
+        width: 100%;
+        height: 100%;
+    }
 	#qrcode{
-		height: 230px;
-		width: 200px;
+		height: 170px;
+		width: 170px;
 		margin: auto;
         position: relative;
+        top: 30%;
 	}
     .logins{
         width: 100%;
         text-align: center;
         position: absolute;
-        top: 70%;
+        top: 60%;
     }
 </style>
