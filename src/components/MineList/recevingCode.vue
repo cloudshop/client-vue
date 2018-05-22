17cc00c0ab17f92034036d44f6b7d14dcbe9e8cc
unknown <13691226692@163.com>
2018-05-22 10:20:27 +0800

    RecevingCode收款码

---
+++ src/components/MineList/recevingCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='recevingCode'>
+    收款码
+  </div>
+  
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.recevingCode{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
