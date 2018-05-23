88738750c29d2ace29519a89dbd4bce1da14ed09
zhang jingjing <13691226692@163.com>
2018-05-23 10:23:10 +0000

    Update recevingCode.vue

--- src/components/MineList/recevingCode.vue
+++ src/components/MineList/recevingCode.vue
@@ -51,18 +51,7 @@ export default {
             })
         },
         back: function(){
-            var  val={
-                "func":"closeCurrent",
-                "param":{'refreshParent':true},
-            };
-            var u = navigator.userAgent;
-            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
-            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-            if(isiOS){
-                window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
-            }else if(isAndroid){  
-                window.androidObject.JSCallAndroid(JSON.stringify(val));
-            }
+           this.$router.go(-1);
         }
     },
 }
