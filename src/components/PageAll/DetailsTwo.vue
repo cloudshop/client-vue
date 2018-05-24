cf406bb59f104cce67894335aa646349a3cfdcb2
zhang jingjing <13691226692@163.com>
2018-05-24 09:17:53 +0000

    Update DetailsTwo.vue

--- src/components/PageAll/DetailsTwo.vue
+++ src/components/PageAll/DetailsTwo.vue
@@ -123,8 +123,21 @@ export default {
         //     }) 
         // },
         details(id){
-          sessionStorage.setItem("GoodsID",id);                 
-          this.$router.push({name:"Product",params:{name:'/DetailsTwo'}})         
+           sessionStorage.setItem("GoodsID",id);                 
+           var  val={
+             "func":"openURL",
+             "param":{
+                  "URL":'/#/Product',
+              },
+             };
+          var u = navigator.userAgent;
+          var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
+          var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+          if(isiOS){
+             window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+          }else if(isAndroid){  
+             window.androidObject.JSCallAndroid(JSON.stringify(val));
+          }         
         },
         seekAll(){
             console.log(this.seekContent)
