1fe01925ec30cd1bf016753c7e54180da4b8b727
qi jianbei <17600045817@163.com>
2018-05-24 14:15:32 +0000

    qijianbei Update change.js

--- h5-src/simpleregister/style/js/change.js
+++ h5-src/simpleregister/style/js/change.js
@@ -1,5 +1,5 @@
 // http: //cloud.eyun.online:9080/verify/api/verify/smscode?phone=1
-$(document).ready(function(){ 
+$(document).ready(function() {
     console.log(localStorage.getItem('token'))
 });
 $('.get').click(function() {
@@ -11,6 +11,7 @@ $('.get').click(function() {
     } else {
         var countdown = 60;
         settime('.gets');
+
         function settime(obj) {
             if (countdown == 0) {
                 $(obj).attr("disabled", false);
@@ -27,14 +28,17 @@ $('.get').click(function() {
             }, 1000)
         }
         $.ajax({
-            url: 'verify/api/verify/smscode?phone=' + tel,
-            method: 'get',
+            url: 'http://app.grjf365.com/api/verify/api/verify/smscode?phone=' + tel,
+            type: 'get',
             callback: 'cb',
             success: function(res) {
-              
+                alert('验证码发送成功')
             },
-            error(res) {
-                alert(res.responseJSON.title)
+            error: function(error) {
+
+                var a = JSON.parse(error.responseText)
+                alert(a.title)
+                console.log(error)
             }
         })
     }
@@ -44,23 +48,34 @@ $('.sure').click(function() {
     var tel = $(".tel").val();
     var yzm = $(".yzm").val();
     var psd = $('.psd').val();
-    var urltel =window.location.search;  
-    var tel2 = urltel.substring(13,24);
+    var urltel = window.location.search;
+    var tel2 = urltel.substring(13, 24);
     var p1 = /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
-    var datas = { 'login': tel, 'password': psd, 'verifyCode': yzm, 'recommend':tel2}
+    // var datas = { 'login': tel, 'password': psd, 'verifyCode': yzm, 'recommend': "17600045817" }
+    var datas = { login: tel, password: psd, verifyCode: yzm, recommend: "17600045817" }
+
     $.ajax({
-        url: 'uaa/api/register/app',
-        method: 'post',
+        // url: 'https://bird.ioliu.cn/v2/?url=http://app.grjf365.com/api/uaa/api/register/app',
+        url: 'http://app.grjf365.com:9080/uaa/api/register/app',
+        type: 'post',
         //callback: 'cb',
+        headers: {
+            //  'Authorization': 'Bearer ' + accessToken,
+            'Content-Type': "application/json"
+        },
+        contentType: 'application/json',
         data: JSON.stringify(datas),
         success: function(res) {
             console.log(res)
             alert('注册成功,更多精彩请下载app体验')
         },
-        error(error) {
-            if (error.response.status === 500) {
-                alert('服务器繁忙，请耐心等待')
-            }
+        error: function(error) {
+            console.log(error)
+            var b = JSON.parse(error.responseText)
+            alert(b.title)
+
+            // alert('服务器繁忙，请耐心等待')
+
         }
     })
 })
