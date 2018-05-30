c03ec995018ae9025bc54edb8bffc3117267effe
zhang jingjing <13691226692@163.com>
2018-05-30 09:24:31 +0000

    Update Shopping.vue

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -354,7 +354,21 @@ export default {
     //         alert(error.response.data.title)
     //       }
     //     });
-        this.$router.push({name:"ConfirmAnOrder"}) 
+    // this.$router.push({name:"ConfirmAnOrder"}) 
+        var val = {
+            func: "openURL",
+            param: {
+                URL: '/#/ConfirmAnOrder',
+            }
+        };
+        var u = navigator.userAgent;
+        var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; // android终端
+        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
+        if (isiOS) {
+            window.webkit.messageHandlers.GongrongAppModel.postMessage(val);
+        } else if (isAndroid) {
+            window.androidObject.JSCallAndroid(JSON.stringify(val));
+        }
       },
       goings(){
         var  val={
