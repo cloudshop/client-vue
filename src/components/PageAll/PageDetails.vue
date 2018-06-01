e6b849855d8101a892a54fef919d9738c983b5a3
zhang xiaowei <weige0088@126.com>
2018-06-01 06:16:42 +0000

    Update PageDetails.vue

--- src/components/PageAll/PageDetails.vue
+++ src/components/PageAll/PageDetails.vue
@@ -37,13 +37,13 @@
             </div>
           </div>
           <div slot="top" class="mint-loadmore-top">
-            <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
+            <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
             <span v-show="topStatus === 'loading'">
               <mt-spinner type="snake"></mt-spinner>
             </span>
           </div>
           <div slot="bottom" class="mint-loadmore-bottom">
-            <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
+            <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
             <span v-show="bottomStatus === 'loading'">
               <mt-spinner type="snake"></mt-spinner>
             </span>
