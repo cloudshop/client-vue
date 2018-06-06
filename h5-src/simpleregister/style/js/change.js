e526a3637825645577e38552a40ef3a3369e60a6
qi jianbei <17600045817@163.com>
2018-06-06 09:54:33 +0000

    qijianbei Update change.js

--- h5-src/simpleregister/style/js/change.js
+++ h5-src/simpleregister/style/js/change.js
@@ -51,48 +51,32 @@ $('.sure').click(function() {
     var urltel = window.location.search;
     var tel2 = urltel.substring(13, 24);
     var p1 = /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
-    // var datas = { 'login': tel, 'password': psd, 'verifyCode': yzm, 'recommend': "17600045817" }
     var datas = { login: tel, password: psd, verifyCode: yzm, recommend: tel2 }
-
-    $.ajax({
-        // url: 'https://bird.ioliu.cn/v2/?url=http://app.grjf365.com/api/uaa/api/register/app',
-        url: 'http://app.grjf365.com/api/uaa/api/register/app',
-        type: 'post',
-        //callback: 'cb',
-        headers: {
-            //  'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': "application/json"
-        },
-        contentType: 'application/json',
-        data: JSON.stringify(datas),
-        success: function(res) {
-            console.log(res)
-            alert('注册成功,更多精彩请下载app体验')
-        },
-        error: function(error) {
-            console.log(error)
-            var b = JSON.parse(error.responseText)
-            alert(b.title)
-
-            // alert('服务器繁忙，请耐心等待')
-
+    if (psd == '' || yzm == '' || tel == '') {
+        alert('请输入完整信息')
+    } else {
+        if (p1.test(tel) == false) {
+            alert('请输入正确手机号')
+        } else {
+            $.ajax({
+                url: 'http://app.grjf365.com/api/uaa/api/register/app',
+                type: 'post',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                contentType: 'application/json',
+                data: JSON.stringify(datas),
+                success: function(res) {
+                    console.log(res)
+                    alert('注册成功,更多精彩请下载app体验')
+                },
+                error: function(error) {
+                    console.log(error)
+                    var b = JSON.parse(error.responseText)
+                    alert(b.title)
+                }
+            })
         }
-    })
-})
-$('.test').click(function() {
-    // var test = window.location.pathname;
-    // alert(test);
-    $(document).ready(function() {
-        var url = window.location.pathname; //你的地址
-        if (url.indexOf("?") != -1) {
-            var str = url.substr(1)
-            strs = str.split("&");
-            for (i = 0; i < strs.length; i++) {
-                alert(strs.length);
-                alert(strs[i].split("=")[0]);
-                alert(strs[i].split("=")[1]);
-                alert(strs[i].split("=")[0], '＝', strs[i].split("=")[1], '');
-            }
-        }
-    })
+    }
+
 })
